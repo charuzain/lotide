e41eeabdc960b8bcbaa057cf840d029e79cf6e96
charuzain <charuabhizain@gmail.com>
2021-09-20 18:27:56 -0400

    refactor EqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else
-    console.log(`⭕ Assertion Failed: ${actual} !== ${expected}`);
-};
-  
-//The Function eqArrays takes in two arrays
-//and returns true or false, based on a perfect match.
 
 const eqArrays = function(array1 , array2) {
   let isEqual = true;
@@ -24,13 +15,9 @@ const eqArrays = function(array1 , array2) {
   }
   return isEqual;
 };
-//console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-//console.log(eqArrays([1, 2, 3], [3, 2, 1]))// => false
-//console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
-//console.log(eqArrays(["1", "2", "3"], ["2", 3]))
-
-// Test Cases
+module.exports = eqArrays;
+/* // Test Cases
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);//when two arrays are equal
 assertEqual(eqArrays([2, 3], [1, 2, 3]), false);//When 2 arrays are of different length
 assertEqual(eqArrays([1, 2, 3], ["1", "2", "3"]), true);//Different Data Type
-assertEqual(eqArrays([], []), true);//Both arrays are empty
+assertEqual(eqArrays([], []), true);//Both arrays are empty*/
