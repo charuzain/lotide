944c346055949ad8dc2c78f2e615398ee8c3f584
charuzain <charuabhizain@gmail.com>
2021-09-20 18:28:56 -0400

    Test Case : head function

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,6 @@
-// Test Case for Head function
 const head = require('../head.js');
 const assertEqual = require('../assertEqual.js');
+
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]), 5);
