3452517eb9e37d9b480d4fd513f7d56eabe9966b
charuzain <charuabhizain@gmail.com>
2021-09-20 22:59:15 -0400

    Test code for head

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,27 @@
+const assert = require('chai').assert;
 const head = require('../head.js');
-const assertEqual = require('../assertEqual.js');
+//const assertEqual = require('../assertEqual.js');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head([2]), 5);
+describe("#head",()=>{
+  it("returns 1 for [1,2,3]" ,()=>{
+    assert.strictEqual(head([1,2,3]),1);
+
+  });
+
+  it("returns '5' for ['5']" ,()=>{
+    assert.strictEqual(head(['5']), '5');
+  
+  });
+
+  it("should not return output as an array " ,()=>{
+    assert.isNotArray(head(['5','3']));
+    
+  });
+
+  it("empty array should return undefined " ,()=>{
+    assert.isUndefined(head([]));
+      
+  });
+  
+
+});
