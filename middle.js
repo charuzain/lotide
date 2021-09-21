c1ff05b425ffe4e1790e413c36dddaaefc38ec36
charuzain <charuabhizain@gmail.com>
2021-09-20 21:55:12 -0400

    middle element of array

--- middle.js
+++ middle.js
@@ -1,48 +1,26 @@
-const eqArrays = function(array1 , array2) {
-  let isEqual = true;
-  
-  if (array1.length === array2.length) {
-  
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        isEqual = false;
-        break;
-      }
-    }
-  } else {
-    isEqual = false; // here i have done that
-  }
-  return isEqual;
-};
 
-const assertArraysEqual = function(array1 , array2) {
-  if (eqArrays(array1,array2)) {
-    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
-  } else
-    console.log(`⭕ Assertion Failed: ${array1} !== ${array2}`);
-};
- 
- 
-const middle = function(array){
-  let middleElement = [];
-  if(array.length == 1 || array.length == 2){
+const middle = function(array) {
+  let middleElement = [ ];
+  if (array.length === 1 || array.length === 2) {
+    return middleElement;
+  } else if (array.length % 2 !== 0) {
+    let arrayIndex = Math.floor(array.length / 2);
+    middleElement.push(array[arrayIndex]);
+    return middleElement;
+  } else if (array.length % 2 === 0) {
+    let arrayIndex = Math.floor((array.length - 1) / 2);
+    middleElement.push(array[arrayIndex]);
+    middleElement.push(array[arrayIndex + 1]);
     return middleElement;
   }
- else if (array.length % 2 !== 0){
-   let arrayIndex = Math.floor(array.length/2)
-   middleElement.push(array[arrayIndex]);
-  return middleElement;
- }
- else if(array.length % 2 === 0){
-  let arrayIndex = Math.floor((array.length-1)/2);
-  middleElement.push(array[arrayIndex]);
-  middleElement.push(array[arrayIndex+1]);
-    return middleElement;
- }
 
-}
-console.log(middle([2,3,4,5,6,7]));
-console.log(middle([2,3,4]));
-console.log(middle([2]));
-console.log(middle([2,3]));
-console.log(middle([]));
+};
+
+module.exports = middle;
+
+/* assertArraysEqual(middle([2,3,4,5,6,7]),[4]);
+assertArraysEqual(middle([2,3,4,5]),[3,4]);
+assertArraysEqual(middle([1,2,3,4,5,6,7,8]),[4,5]);
+assertArraysEqual(middle([1]),[]);
+assertArraysEqual(middle([2,3]),[4]);
+assertArraysEqual(middle([2,3]),[]);*/
