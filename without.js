63ee265a53e35650725f73014c90168344fb7925
charuzain <charuabhizain@gmail.com>
2021-09-21 23:14:13 -0400

    without function

--- without.js
+++ without.js
@@ -1,27 +1,3 @@
-const eqArrays = function(array1 , array2) {
-  let isEqual = true;
-  
-  if (array1.length === array2.length) {
-  
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        isEqual = false;
-        break;
-      }
-    }
-  } else {
-    isEqual = false; // here i have done that
-  }
-  return isEqual;
-};
-
-const assertArraysEqual = function(array1 , array2) {
-  if (eqArrays(array1,array2)) {
-    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
-  } else
-    console.log(`⭕ Assertion Failed: ${array1} !== ${array2}`);
-}; 
-
 const without = function(source,itemsToRemove){
 let newArray =[];
 let itemExist = false;
@@ -45,8 +21,37 @@ for(let i = 0 ; i<source.length;i++){
 return newArray;
 }
 
+module.exports = without;
+
+
+
+
+/* const eqArrays = function(array1 , array2) {
+  let isEqual = true;
+  
+  if (array1.length === array2.length) {
+  
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        isEqual = false;
+        break;
+      }
+    }
+  } else {
+    isEqual = false; // here i have done that
+  }
+  return isEqual;
+};
+
+const assertArraysEqual = function(array1 , array2) {
+  if (eqArrays(array1,array2)) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else
+    console.log(`⭕ Assertion Failed: ${array1} !== ${array2}`);
+}; 
+
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); 
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-assertArraysEqual(without([1,2,3,4],[1,2]), [3,4]);
+assertArraysEqual(without([1,2,3,4],[1,2]), [3,4]); */
