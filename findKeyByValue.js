e0b6599f9db2c2fd5dcf69be8d318a8741e18777
charuzain <charuabhizain@gmail.com>
2021-09-21 23:17:43 -0400

    findKeyByvalue function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected){
-  if (actual === expected){
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  else
-  console.log(`⭕ Assertion Failed: ${actual} !== ${expected}`);
-  };
 
  const findKeyByValue = function(object,value){
   let keys = Object.keys(object);
@@ -15,8 +8,21 @@ const assertEqual = function(actual, expected){
   }
   return undefined;
  }
+ module.exports = findKeyByValue;
 
+ 
+ /* 
+ const assertEqual = function(actual, expected){
+  if (actual === expected){
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  else
+  console.log(`⭕ Assertion Failed: ${actual} !== ${expected}`);
+  };
+
+ 
  //Test Cases
+
  const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
@@ -26,3 +32,4 @@ const assertEqual = function(actual, expected){
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
+*/
