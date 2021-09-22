ac729c6861623b129397634c84e6909d35fce982
charuzain <charuabhizain@gmail.com>
2021-09-21 23:14:39 -0400

    takeUntil finction

--- takeUntil.js
+++ takeUntil.js
@@ -11,32 +11,5 @@ for(let elem of array){
 }
 return result;
 }
-//console.log(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5],x => x < 0));
-//console.log(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5],x => x < 0));
-console.log(takeUntil(["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"], x => x === ','));
 
-const eqArrays = function(array1 , array2) {
-  let isEqual = true;
-  
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        isEqual = false;
-        break;
-      }
-    }
-  } else {
-    isEqual = false; 
-  }
-  return isEqual;
-};
-
-const assertArraysEqual = function(array1 , array2) {
-  if (eqArrays(array1,array2)) {
-    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
-  } else
-    console.log(`⭕ Assertion Failed: ${array1} !== ${array2}`);
-};
-
-assertArraysEqual(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5],x => x < 0),[3,2,3,4,3])
-assertArraysEqual(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5],x => x < 0),[1,2,5,7,2])
+module.exports = takeUntil;
